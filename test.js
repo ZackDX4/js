9d12929e9699386174dc89377b0387c7e06c2f48
ZackDX4 <112519022+ZackDX4@users.noreply.github.com>
2024-07-02 14:33:11 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,4 @@
+function arena() {
+  document.getElementById("mastrotv").innerHTML =
+    ' <iframe src="intent://linearjitp-playback.astro.com.my/dash-wv/linear/2604/default_primary.mpd|Referrer=https://astrogo.astro.com.my/&drmScheme=clearkey&drmLicense=efda647dad723563b625be8c05ee6110:6776985938b332164b97b81fe566a34a#Intent;scheme=https;type=video/*;package=com.google.android.exoplayer2.demo;S.browser_fallback_url=market://details?id=com.google.android.exoplayer2.demo;S.title=MASTROTV;end"></iframe>';
+}
